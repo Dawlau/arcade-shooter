7413ea67ff9b079533bf45e1aae1dceb9bd7de24
Dawlau <blahoviciandrei1@gmail.com>
2021-12-08 23:35:39 +0200

    added settings for displays

--- arcade_shooter/arcade_shooter.ino
+++ arcade_shooter/arcade_shooter.ino
@@ -1,7 +1,8 @@
 // TO DO:
-// create a scrolling text function
-// maybe delete the write text at pos function
-// create movement function for joystick on both axis
+// make matrix work
+// add naming
+// test matrix brightness
+// add highscores to eeprom
 
 #include "LedControl.h"
 #include <LiquidCrystal.h>
@@ -39,14 +40,38 @@ const byte selectionArrow[8] = {
   B10000
 };
 
+const byte increaseArrow[8] = {
+  B01000,
+  B01100,
+  B01110,
+  B01111,
+  B01110,
+  B01100,
+  B01100,
+  B01000
+};
+
+const byte decreaseArrow[8] = {
+  B00010,
+  B00110,
+  B01110,
+  B11110,
+  B01110,
+  B00110,
+  B00010,
+  B00000
+};
+
 const int downScrollingArrowLcdId = 0;
 const int upScrollingArrowLcdId = 1;
 const int selectionArrowLcdId = 2;
+const int decreaseArrowLcdId = 3;
+const int increaseArrowLcdId = 4;
 
 const int RS = 8;
 const int enable = 9;
 const int D4 = 7;
-const int D5 = 6;
+const int D5 = 13;
 const int D6 = 5;
 const int D7 = 4;
 
@@ -58,16 +83,21 @@ const int joystickPinX = A0;
 const int joystickPinY = A1;
 const int joystickPinSW = 2;
 
+const int brightnessPin = 6;
+
 const int matrixSize = 8;
 
 const int joystickMinThreshold = 200;
-const int joystickMaxThreshold = 850;
+const int joystickMaxThreshold = 750;
 
 const int contrastPin = 3;
 
 const String gameName = "Arcade-shooter";
 
 const int maxHighscoresCount = 3;
+
+const int minLevel = 1;
+const int maxLevel = 6;
 
 String highscoreNames[maxHighscoresCount] = {
   "aaaa",
@@ -88,7 +118,14 @@ byte yPos = 0;
 byte xLastPos = 0;
 byte yLastPos = 0;
 
-int matrixBrightness = 15;
+const int minMatrixBrightness = 1;
+int matrixBrightness = 8;
+const int maxMatrixBrightness = 9;
+
+const int minBrightness = 10;
+int brightness = 240;
+const int maxBrightness = 245;
+const int brightnessOffset = 10;
 
 
 const int lcdWidth = 16;
@@ -97,9 +134,12 @@ const int lcdHeight = 2;
 unsigned long long lastMoved = 0;
 const int moveInterval = 100;
 
+const unsigned int minContrast = 60;
 unsigned int contrast = 110;
+const unsigned int maxContrast = 150;
 
 bool matrixUpdate = true;
+int level = minLevel;
 
 LedControl lc = LedControl(dinPin, clockPin, loadPin, 1); //DIN, CLK, LOAD, No. DRIVER
 LiquidCrystal lcd(RS, enable, D4, D5, D6, D7);
@@ -111,12 +151,19 @@ enum state {
   highscore,
   about,
   settings,
-  play
+  play,
+  changename,
+  setlevel,
+  setcontrast,
+  setbrightness,
+  setmatrixbrightness
 };
 
 enum joystickMove {
   up,
   down,
+  left,
+  right,
   neutral
 };
 
@@ -162,39 +209,85 @@ void setup() {
 
   pinMode(contrastPin, OUTPUT);
 
+  pinMode(brightnessPin, OUTPUT);
+
+  analogWrite(contrastPin, contrast);
+  analogWrite(brightnessPin, brightness);
+
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 15);
+  lc.clearDisplay(0);
+
   gameState = welcomeScreen;
   lcd.begin(lcdWidth, lcdHeight);
-  analogWrite(contrastPin, contrast);
   lcd.createChar(downScrollingArrowLcdId, downScrollingArrow);
   lcd.createChar(upScrollingArrowLcdId, upScrollingArrow);
   lcd.createChar(selectionArrowLcdId, selectionArrow);
+  lcd.createChar(increaseArrowLcdId, increaseArrow);
+  lcd.createChar(decreaseArrowLcdId, decreaseArrow);
+
+  for (int row = 0; row < matrixSize; row++) {
+    for (int col = 0; col < matrixSize; col++) {
+      matrix[row][col] = 1;
+    }
+  }
+//
+//  for (int row = 0; row < matrixSize; row++) {
+//    for (int col = 0; col < matrixSize; col++) {
+//      lc.setLed(0, row, col, true);
+//    }
+//  }
 }
 
 
 void loop() {
+//
+//  for (int row = 0; row < matrixSize; row++) {
+//    for (int col = 0; col < matrixSize; col++) {
+//      lc.setLed(0, col, row, true);// turns on LEDat col, row
+//      delay(25);
+//    }
+//  } 
+//  for (int row = 0; row < matrixSize; row++) {
+//    for (int col = 0; col < matrixSize; col++) {
+//      lc.setLed(0, col, row, false);// turns offLED at col, row
+//      delay(25);
+//    }
+//  }
 
-  analogWrite(contrastPin, contrast);
 
-  if (gameState == welcomeScreen) {
-    runWelcomeScreen();
-  }
-  else if (gameState == homeScreen) {
-    runHomeScreen();
-  }
-  else if (gameState == highscore) {
-    runHighscoreMenu();
-  }
-  else if (gameState == settings) {
-    runSettingsMenu();
-  }
-  else if (gameState == about) {
-    runAboutMenu();
-  }
-  else {
+if (gameState == welcomeScreen) {
+  runWelcomeScreen();
+}
+else if (gameState == homeScreen) {
+  runHomeScreen();
+}
+else if (gameState == highscore) {
+  runHighscoreMenu();
+}
+else if (gameState == settings) {
+  runSettingsMenu();
+}
+else if (gameState == about) {
+  runAboutMenu();
+}
+else if (gameState == changename) {
+  runChangeName();
+}
+else if (gameState == setlevel) {
+  runSetLevel();
+}
+else if (gameState == setcontrast) {
+  runSetContrast();
+}
+else if (gameState == setbrightness) {
+  runSetBrightness();
+}
+else if (gameState == setmatrixbrightness) {
+  runSetMatrixBrightness();
+}
 
-  }
-
-  lastJoystickSwState = joystickSwState;
+lastJoystickSwState = joystickSwState;
 }
 
 void runWelcomeScreen() {
@@ -331,13 +424,14 @@ void runHomeScreen() {
   int exitCode = renderScrollingMenu(options, optionsLength, true);
 
   if (exitCode != -1) {
-    if (exitCode == 0) {
+    exitCode++;
+    if (exitCode == 1) {
       gameState = play;
     }
-    else if (exitCode == 1) {
+    else if (exitCode == 2) {
       gameState = settings;
     }
-    else if (exitCode == 2) {
+    else if (exitCode == 3) {
       gameState = highscore;
     }
     else {
@@ -365,6 +459,40 @@ void runAboutMenu() {
 
 void runSettingsMenu() {
 
+  static const int settingsCount = 6;
+  static String settings[settingsCount] = {
+    "1.Enter name",
+    "2.Level",
+    "3.Contrast",
+    "4.Brightness",
+    "5.Game light",
+    "6.Back"
+  };
+
+  int exitCode = renderScrollingMenu(settings, settingsCount, true);
+
+  if (exitCode != -1) {
+    exitCode++;
+  }
+
+  if (exitCode == 6) {
+    gameState = homeScreen;
+  }
+  else if (exitCode == 1) {
+    gameState = changename;
+  }
+  else if (exitCode == 2) {
+    gameState = setlevel;
+  }
+  else if (exitCode == 3) {
+    gameState = setcontrast;
+  }
+  else if (exitCode == 4) {
+    gameState = setbrightness;
+  }
+  else if (exitCode == 5) {
+    gameState = setmatrixbrightness;
+  }
 }
 
 int digitsCount(int no) {
@@ -389,11 +517,11 @@ void runHighscoreMenu() {
       String spaces = "";
       int spacesCount = lcdWidth - digitsCount(highscores[i]) - 1 - highscoreNames[i].length();
 
-      while(spacesCount){
+      while (spacesCount) {
         spaces += " ";
         spacesCount--;
       }
-      
+
       lcdContents[i] = highscoreNames[i] + spaces + String(highscores[i]);
     }
 
@@ -401,4 +529,205 @@ void runHighscoreMenu() {
   }
 
   int exitCode = renderScrollingMenu(lcdContents, lcdContentsLength);
+}
+
+joystickMove joystickHorizontalMove() {
+
+  static unsigned long long lastChange = 0;
+  static unsigned int updateInterval = 200;
+
+  if (millis() - lastChange > updateInterval) {
+
+
+    int yValue = analogRead(joystickPinY);
+
+    if (yValue < joystickMinThreshold) { // joystick moved right
+      lastChange = millis();
+      return right;
+    }
+
+    if (yValue > joystickMaxThreshold) { // joystick moved left
+      lastChange = millis();
+      return left;
+    }
+  }
+
+  return neutral;
+}
+
+void runChangeName() {
+
+
+}
+
+void runSetLevel() {
+
+  static int lastLevel = 0;
+
+  int joystickMove = joystickHorizontalMove();
+
+  if (joystickMove == left && level > minLevel) {
+    level--;
+  }
+
+  if (joystickMove == right && level < maxLevel) {
+    level++;
+  }
+
+
+  String title = "Select level:";
+
+  if (level != lastLevel) {
+    lcd.clear();
+
+    lcd.home();
+    lcd.print(title);
+
+    lcd.setCursor(lcdWidth / 2 - 2, 1);
+    lcd.write(byte(decreaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2 + 2, 1);
+    lcd.write(byte(increaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2, 1);
+    lcd.print(level);
+  }
+
+  lastLevel = level;
+
+  if (joystickSwState != lastJoystickSwState) {
+    lastLevel = 0;
+    gameState = settings;
+  }
+}
+
+void runSetContrast() {
+
+  static int lastContrast = 0;
+
+  int joystickMove = joystickHorizontalMove();
+
+  if (joystickMove == left && contrast > minContrast) {
+    contrast--;
+  }
+
+  if (joystickMove == right && contrast < maxContrast) {
+    contrast++;
+  }
+
+
+
+  String title = "Select contrast:";
+
+  if (contrast != lastContrast) {
+    lcd.clear();
+
+    lcd.home();
+    lcd.print(title);
+
+    lcd.setCursor(lcdWidth / 2 - 2, 1);
+    lcd.write(byte(decreaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2 + 2, 1);
+    lcd.write(byte(increaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2, 1);
+    lcd.print(contrast - minContrast);
+
+    analogWrite(contrastPin, contrast);
+  }
+
+  lastContrast = contrast;
+
+  if (joystickSwState != lastJoystickSwState) {
+    lastContrast = 0;
+    gameState = settings;
+  }
+}
+
+void runSetBrightness() {
+
+  static int lastBrightness = 0;
+
+  int joystickMove = joystickHorizontalMove();
+
+  if (joystickMove == left && brightness > minBrightness) {
+    brightness -= brightnessOffset;
+  }
+
+  if (joystickMove == right && brightness < maxBrightness) {
+    brightness += brightnessOffset;
+  }
+
+  String title = "Brightness:";
+
+  if (brightness != lastBrightness) {
+    lcd.clear();
+
+    lcd.home();
+    lcd.print(title);
+
+    int startPosition = lcdWidth - 4 - digitsCount(brightness - minBrightness);
+    startPosition /= 2;
+
+    lcd.setCursor(startPosition, 1);
+    lcd.write(byte(decreaseArrowLcdId));
+
+    lcd.setCursor(startPosition + digitsCount(brightness - minBrightness) + 3, 1);
+    lcd.write(byte(increaseArrowLcdId));
+
+    lcd.setCursor(startPosition + 2, 1);
+    lcd.print(brightness - minBrightness);
+
+    analogWrite(brightnessPin, brightness);
+  }
+
+  lastBrightness = brightness;
+
+  if (joystickSwState != lastJoystickSwState) {
+    lastBrightness = 0;
+    gameState = settings;
+  }
+}
+
+void runSetMatrixBrightness() {
+
+  static int lastMatrixBrightness = 0;
+
+  int joystickMove = joystickHorizontalMove();
+
+  if (joystickMove == left && matrixBrightness > minMatrixBrightness) {
+    matrixBrightness--;
+  }
+
+  if (joystickMove == right && matrixBrightness < maxMatrixBrightness) {
+    matrixBrightness++;
+  }
+
+  String title = "Game brightness:";
+
+  if (matrixBrightness != lastMatrixBrightness) {
+    lcd.clear();
+
+    lcd.home();
+    lcd.print(title);
+
+    lcd.setCursor(lcdWidth / 2 - 2, 1);
+    lcd.write(byte(decreaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2 + 2, 1);
+    lcd.write(byte(increaseArrowLcdId));
+
+    lcd.setCursor(lcdWidth / 2, 1);
+    lcd.print(matrixBrightness);
+
+    lc.setIntensity(0, matrixBrightness);
+  }
+
+  lastMatrixBrightness = matrixBrightness;
+
+  if (joystickSwState != lastJoystickSwState) {
+    lastMatrixBrightness = 0;
+    gameState = settings;
+  }
 }
